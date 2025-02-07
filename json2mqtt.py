84a91f01291070f87dc3b5b15639d5c4a26ff8f7
Jesse Kleemann <j.kleemann@tu-bs.de>
2025-02-07 01:00:36 +0100

    run async

--- json2mqtt.py
+++ json2mqtt.py
@@ -1,24 +1,33 @@
+import asyncio
 import json
 import logging
 import signal
-import sys
 import time
 from functools import reduce
 
-import requests
-from urllib3.exceptions import MaxRetryError, NameResolutionError
+import httpcore
+from httpcore import ConnectError
 
 from config import get_first_config
 from mqtt_handler import MqttHandler
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 
 class Json2Mqtt:
     def __init__(self):
-        signal.signal(signal.SIGINT, self.exit_handler)
-        signal.signal(signal.SIGTERM, self.exit_handler)
         config = get_first_config()
+        self.setup_logging(config)
+        self.mqtt_handler = MqttHandler(config)
+        self.headers: dict = config.get('headers')
+        self.requests: dict = config['requests']
+        self.update_rate: int = config.get('update_rate', 600)
+
+    async def exit(self):
+        await self.mqtt_handler.disconnect()
+
+    @staticmethod
+    def setup_logging(config):
         if 'logging' in config:
             logging_level_name = config['logging'].upper()
             logging_level = logging.getLevelNamesMapping().get(logging_level_name, logging.NOTSET)
@@ -26,42 +35,62 @@ class Json2Mqtt:
                 logging.getLogger().setLevel(logging_level)
             else:
                 logging.warning('unknown logging level: %s.', logging_level)
-        self.mqtt_handler = MqttHandler(config)
-        self.headers: dict = config.get('headers')
-        self.requests: dict = config['requests']
-        self.update_rate: int = config.get('update_rate', 600)
 
-    @staticmethod
-    def exit_handler(signum, frame):
-        logging.info('stopping Json2Mqtt.')
-        sys.exit(0)
+    async def loop_iteration(self) -> None:
+        if not await self.mqtt_handler.connect():
+            return
+        for url, request in self.requests.items():
+            value = json.loads(await self.fetch(url))
+            value = reduce(lambda d, key: d[key], request['path'], value)
+            self.mqtt_handler.publish(request['topic'], value, request.get('retain', False))
 
-    def loop(self):
+    async def loop(self) -> None:
         while True:
-            start_time = time.perf_counter()
-            for url, request in self.requests.items():
-                try:
-                    response = requests.get(url, headers=self.headers)
-                    value = json.loads(response.text)
-                    value = reduce(lambda d, key: d[key], request['path'], value)
-                    self.mqtt_handler.publish(request['topic'], value, request.get('retain', False))
-                except MaxRetryError as e:
-                    logging.error('%s max retries exceeded: %s', url, e)
-                except NameResolutionError as e:
-                    logging.error('%s failed to resolve hostname: %s', url, e)
-                except ConnectionError as e:
-                    logging.error('%s connection failed: %s', url, e)
-                except Exception as e:
-                    logging.error('%s failed: %s', url, e)
-            time_taken = time.perf_counter() - start_time
-            time_to_sleep = self.update_rate - time_taken
+            start_time: float = time.perf_counter()
+            await self.loop_iteration()
+            time_taken: float = time.perf_counter() - start_time
+            time_to_sleep: float = self.update_rate - time_taken
             logging.debug('looped in %.2fms, sleeping %.2fs.', time_taken * 1000, time_to_sleep)
             if time_to_sleep > 0:
-                time.sleep(time_to_sleep)
+                await asyncio.sleep(time_to_sleep)
+
+    async def fetch(self, url: str) -> str:
+        async with httpcore.AsyncConnectionPool() as http:
+            try:
+                response = await http.request("GET", url, headers=self.headers)
+                return response.content.decode()
+            except ConnectError as e:
+                logging.warning(f'{e=}, {url=}')
+            except Exception as e:
+                logging.error(f'{e=}, {url=}')
+            return ''
+
+
+async def main():
+    app = Json2Mqtt()
+    loop = asyncio.get_running_loop()
+    main_task = asyncio.current_task()
+
+    def shutdown_handler():
+        if not main_task.done():
+            main_task.cancel()
+
+    try:
+        for sig in (signal.SIGINT, signal.SIGTERM):
+            loop.add_signal_handler(sig, shutdown_handler)
+    except NotImplementedError:
+        pass
+    try:
+        await app.loop()
+    except asyncio.CancelledError:
+        logging.info('exiting.')
+    finally:
+        await app.exit()
+        logging.info('exited.')
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.getLogger('gmqtt').setLevel(logging.ERROR)
     logging.info('starting Json2Mqtt v%s.', __version__)
-    app = Json2Mqtt()
-    app.loop()
+    asyncio.run(main())
--- mqtt_handler.py
+++ mqtt_handler.py
@@ -1,51 +1,39 @@
 import logging
-import queue
-import threading
-from time import sleep
 
-import paho.mqtt.client as mqtt
-from paho.mqtt.packettypes import PacketTypes
-from paho.mqtt.reasoncodes import ReasonCode
+from gmqtt import Client as MQTTClient, Message
 
 
 class MqttHandler:
-    def __init__(self, config: dict):
+    def __init__(self, config: dict) -> None:
         self.topic_prefix: str = config.get('mqtt_topic', 'json2mqtt/').rstrip('/') + '/'
         self.host: str = config['mqtt_server']
         self.port: int = config.get('mqtt_port', 1883)
 
-        self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        will_message: Message = Message(self.topic_prefix + 'available', 'offline', will_delay_interval=5, retain=True)
+        self.mqttc: MQTTClient = MQTTClient(client_id=None, will_message=will_message)
         self.mqttc.on_connect = self.on_connect
-        self.mqttc.username_pw_set(config['mqtt_username'], config['mqtt_password'])
-        self.mqttc.will_set(self.topic_prefix + 'available', 'offline', retain=True)
-        self.mqttc.connect_async(host=self.host, port=self.port)
-        self.mqttc.loop_start()
+        self.mqttc.set_auth_credentials(config['mqtt_username'], config['mqtt_password'])
 
-        self.publishing_queue = queue.Queue()
+    def on_connect(self, client, flags, rc, properties):
+        client.publish(self.topic_prefix + 'available', 'online', retain=True)
+        logging.info('mqtt connected.')
 
-        self.publishing_thread = threading.Thread(target=self.publishing_handler, daemon=True)
-        self.publishing_thread.start()
+    def publish(self, topic: str, payload: str | int | float, retain: bool = False) -> None:
+        self.mqttc.publish(self.topic_prefix + topic, payload, retain=retain)
 
-    def on_connect(self, client, userdata, connect_flags, reason_code, properties):
-        self.mqttc.publish(self.topic_prefix + 'available', 'online', retain=True)
-        if reason_code == ReasonCode(PacketTypes.CONNACK, 'Success'):
-            logging.info('mqtt connected.')
-        else:
-            logging.error(f'mqtt connection to %s:%s failed, %s.', self.host, self.port, reason_code)
+    async def connect(self) -> bool:
+        if self.mqttc.is_connected:
+            return True
+        try:
+            await self.mqttc.connect(self.host, self.port)
+            return True
+        except ConnectionRefusedError as e:
+            logging.warning(f"mqtt: {self.host=}, {e=}")
+        except Exception as e:
+            logging.error(f"mqtt: {self.host=}, {e=}")
+        return False
 
-    def publish(self, topic: str, payload: str | int | float, retain=False):
-        self.publishing_queue.put({
-            'topic': self.topic_prefix + topic,
-            'payload': payload,
-            'retain': retain,
-        })
-
-    def publishing_handler(self):
-        while True:
-            message: dict = self.publishing_queue.get()
-            while not self.mqttc.is_connected():
-                sleep(1)
-            result = self.mqttc.publish(**message)
-            if result.rc != mqtt.MQTT_ERR_SUCCESS:
-                logging.error(f'mqtt publish failed: %s %s.', message, result)
-            self.publishing_queue.task_done()
+    async def disconnect(self):
+        if self.mqttc.is_connected:
+            await self.mqttc.disconnect(reason_code=4)
+            logging.info('mqtt disconnected.')
